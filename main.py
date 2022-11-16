db5cc1ec7a0ae6d6c26a73a5341a74d4dc499c53
Timothee Muzaliwa <tmuzaliwwa@gmail.com>
2022-11-16 19:56:27 +0300

    - add routes like search location - serialize records

--- main.py
+++ main.py
@@ -26,7 +26,21 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    # -------------- render result to a dictionary --------------- #
+    def to_dict(self):
+        # method 1 : Loop through each element in the data row
+        # dictionary = {}
+        # for column in self.__table__.columns:
+        #     """ Create a new dictionary entry; where the key is the name of the column
+        #     and the value is the value of the column"""
+        #     dictionary[column.name] = getattr(self, column.name)
+        # return dictionary
 
+        # method 2: Uses dictionary comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+# --------------- All routes --------------- #
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -35,15 +49,34 @@ def home():
 @app.route('/random')
 def get_random_cafe():
     # return random record object from database using built functions
-    random_cafe = db.session.query(Cafe).order_by(func.random()).first()
-    # cafes = choice(db.session.query(Cafe).all())  # Does the same thing with python syntax
-    cafe_dictionary = random_cafe.__dict__
-    del cafe_dictionary["_sa_instance_state"]
-    print(cafe_dictionary)
-    return jsonify(cafe_dictionary)
+    # random_cafe = db.session.query(Cafe).order_by(func.random()).first()
+    random_cafe = choice(db.session.query(Cafe).all())  # Does the same thing with python syntax
+    # cafe_dictionary = random_cafe.__dict__
+    # print(cafe_dictionary)
+    # del cafe_dictionary["_sa_instance_state"]
+    # return jsonify(cafe_dictionary)
+
+                    # or
+
+    # convert the random_cafe data record to a dictionary of key-value pairs
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route('/all')
+def get_cafes():
+    all_cafes = Cafe.query.all()
+    return jsonify(cafe=[cafe.to_dict() for cafe in all_cafes])
 
 
 # HTTP GET - Read Record
+@app.route('/search')
+def search_cafe_location():
+    query_location = request.args.get('loc')
+    cafes = Cafe.query.filter_by(location=query_location).all()
+    if cafes:
+        return jsonify(cafe=[cafe.to_dict() for cafe in cafes])
+    else:
+        return jsonify(error={"Error": "Sorry, we don't have a cafe at the that location."})
 
 # HTTP POST - Create Record
 
